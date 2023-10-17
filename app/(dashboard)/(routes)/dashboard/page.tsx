bd688b2f6b3a6fce99006f4af5b54d0ab7663095
Muhamad Naufal Burhanuddin Balit <67968500+Muhamad-naufal@users.noreply.github.com>
2023-10-17 22:57:18 +0700

    ubah

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button variant="destructive">Button</Button>
+    </div>
+  );
+}
+
+export default DashboardPage;
